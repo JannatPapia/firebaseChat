fba006f0d9a48b98ac7007dffd4711e09ca71e7a
Jannatpapia <papiamcode@gmail.com>
2022-04-18 02:01:26 +0600

    project setup

---
+++ firebaseChat/firebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebaseChatApp.swift
+//  firebaseChat
+//
+//  Created by Jannatun Nahar Papia  on 18/4/22.
+//
+
+import SwiftUI
+
+@main
+struct firebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
