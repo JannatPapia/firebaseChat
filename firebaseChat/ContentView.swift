9ef2c8bfc7de7ddd2962e579127b507b8864eade
Jannatpapia <papiamcode@gmail.com>
2022-04-18 02:18:33 +0600

    Creating a Login Page

--- firebaseChat/ContentView.swift
+++ firebaseChat/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isLoginMode: Bool = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView {
+                Picker(selection: $isLoginMode, label: Text("Picker here")){
+                    Text("Login")
+                        .tag(true)
+                    Text("Create Account")
+                        .tag(false)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding()
+                
+                
+                Text("Here is my creation account page")
+            }
+            .navigationTitle("Create Account")
+        }
     }
 }
 
