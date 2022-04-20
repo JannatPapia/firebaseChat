cc01385e7b12d5c623a7e1e01a15022cfc043fc0
Jannatpapia <papiamcode@gmail.com>
2022-04-20 22:51:51 +0600

    creating a new ChatApp

---
+++ firebaseChat/ChatApp/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  firebaseChat
+//
+//  Created by Jannatun Nahar Papia  on 20/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+        List {
+            ForEach(0 ..< 10) { i in
+                Text(String(i))
+            }
+        }
+        .listStyle(PlainListStyle())
+        .navigationTitle("Chats")
+        .navigationBarItems(trailing: Button(action: {})
+            {
+                Image(systemName: "square.and.pencil")
+            })
+    }
+}
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
